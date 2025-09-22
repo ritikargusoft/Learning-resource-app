132a9a59ce0ca9e90cf2a72115a77b92f63e12b1
Ritik Negi <negiritik47@gmail.com>
2025-09-22 17:44:44 +0530

    creating header and card looks

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,50 @@
 <template>
-  <ul>
-    <learning-resources
-      v-for="res in storedResources"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    ></learning-resources>
-  </ul>
-</template>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>å
 
 <script>
-import LearningResource from "./components/learning-resources/LearningResource.vue";
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
 export default {
   components: {
-    LearningResource,
+    StoredResources,
+    TheHeader,
   },
   data() {
     return {
       storedResources: [
         {
-          id: "official-guide",
-          title: "Official Guide",
-          description: "The official Vue.js documentation.",
-          link: "https://vuejs.org",
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
         },
         {
-          id: "google",
-          title: "Google",
-          description: "Learn to google...",
-          link: "https://google.org",
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
         },
       ],
     };
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
